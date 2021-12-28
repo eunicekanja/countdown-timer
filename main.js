1507ad62b231143b68b4c2bf4a84d0dfc46de70a
Eunice Kanja <kanjawambui30@gmail.com>
2021-12-29 00:44:54 +0300

    init

--- main.js
+++ main.js
@@ -2,7 +2,7 @@
 const text=document.querySelectorAll('.card p')
 const cards=document.querySelectorAll('.card')
 const container=document.querySelector('.main')
-const futureDate=new Date(2022,02,7,11,30,0)
+const futureDate=new Date(2022,2,7,11,30,0)
 console.log(futureDate)
 
 const getDifference=function(){
